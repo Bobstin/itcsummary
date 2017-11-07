dbb7edbbea6feb0078744cfbae395a4ff16a077d
Bobstin <justinakahn@gmail.com>
2017-11-07 13:30:52 -0500

    next attempt at page breaks

--- ITC2017Summary/settings.py
+++ ITC2017Summary/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'ITC2017Summary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'itcsummary',                      
+        'USER': 'itcuser',
+        'PASSWORD': 'testpass',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
